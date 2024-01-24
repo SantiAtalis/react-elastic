2d5a1ab5616dadd1270d4c636115042c780479df
SantiAtalis <santiago@atalisfunding.com>
2024-01-24 10:48:41 +0100

    [feat]: implemented a search by title approach instead of default title search

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 function App() {
   const [data, setData] = useState([])
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('eos enim sit tenetur quia accusamus nobis animi nesciunt laborum pariatur')
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState([])
@@ -16,6 +16,14 @@ function App() {
   // LOCALHOST ENV
   const URL = 'http://46.137.120.82:9200/test_inject_borra/_search' || 'http://127.0.0.1:4567/test/'
   const ELASTIC_LOCAL_URL = 'http://localhost:9200/_security/user/'
+
+  const basicQuery = {
+    "query": {
+      "match": {
+        "title": search
+      }
+    }
+  }
 
   // AUTH METHODS
   // const handleAuth = async () => {
@@ -87,11 +95,43 @@ function App() {
   const handleTitle = async () => {
     setInitialTime(new Date().getTime())
     try {
-      const response = await fetch(URL)
+      const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "query": {
+            "match": {
+              "title": "eos enim sit tenetur quia accusamus nobis animi nesciunt laborum pariatur"
+            }
+          }
+        }),
+      })
       const data = await response.json()
       setData(data)
     } catch (error) {
       console.log(error)
+    } finally {
+      setEndTime(new Date().getTime())
+    }
+  }
+
+  const handleSearch = async () => {
+    setInitialTime(new Date().getTime())
+    try {
+      const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(basicQuery),
+      })
+
+      const data = await response.json()
+      setData(data)
+    } catch (error) {
+      console.error(error);
     } finally {
       setEndTime(new Date().getTime())
     }
@@ -115,18 +155,19 @@ function App() {
   //     setEndTime(new Date().getTime())
   //   }
   // }
-  const handleSearch = async () => {
-    setInitialTime(new Date().getTime())
-    try {
-      const response = await fetch(`${URL}${search}`)
-      const data = await response.json()
-      setData(data)
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setEndTime(new Date().getTime())
-    }
-  }
+ 
+  // const handleSearch = async () => {
+  //   setInitialTime(new Date().getTime())
+  //   try {
+  //     const response = await fetch(`${URL}${search}`)
+  //     const data = await response.json()
+  //     setData(data)
+  //   } catch (error) {
+  //     console.log(error)
+  //   } finally {
+  //     setEndTime(new Date().getTime())
+  //   }
+  // }
 
 
 
@@ -145,8 +186,8 @@ function App() {
           <span>{JSON.stringify(user)}</span>
         </div> */}
         <div className="flex flex-col gap-2 w-full">
-          <input type="text" className="border rounded-sm p-2 text-slate-800" onChange={handleChange} />
-          <button className=" bg-green-600 py-1 rounded-md" onClick={handleSearch}>Searh by ID</button>
+          <input type="text" className="border rounded-sm p-2 text-slate-800" value={search} onChange={handleChange} />
+          <button className=" bg-green-600 py-1 rounded-md" onClick={handleSearch}>Searh by title</button>
           <button className=" bg-green-700 py-1 rounded-md" onClick={handleTitle}>Search by default title</button>
           <button className=" bg-green-800  00 py-1 rounded-md" onClick={searchToElasticRemote}>Search by default criteria Elastic Cloud</button>
         </div>
