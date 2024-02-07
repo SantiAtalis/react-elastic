948f64d9c6cef7a77b4991b1be799ce376c1bc76
MaciaAtalis <macia@atalisfunding.com>
2024-02-07 12:46:57 +0100

    [try]: follow instruction

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/react-elastic/',
   server: {
     port: 5000,
-    host: true,
-    base: '/REACT-ELASTIC/'
+    host: true
   },
   build: {
     outDir: 'dist'
