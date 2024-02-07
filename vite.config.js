dcf8d6dc43d2c8bbf7b42ab3fe0d69ff0ce61cf3
MaciaAtalis <macia@atalisfunding.com>
2024-02-07 12:51:32 +0100

    [try]: try add simple config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base:'/react-elastic/',
-  server: {
-    port: 5000,
-    host: true
-  },
-  build: {
-    outDir: 'dist'
-  }
-})
+});
